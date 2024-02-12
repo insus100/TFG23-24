26cd540aeb4b824bb9c35ae8c95899ccc8a5cf0a
MigueldeAreba <mdeareba@ucm.es>
2024-02-12 10:51:48 +0100

    fuente blanca

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -38,7 +38,7 @@ function LoginPage() {
     <div className="justify-center h-[calc(100vh-4rem)] flex items-center">
       <form onSubmit={handleSubmit} className="bg-neutral-950 px-8 py-10 w-3/12">
         {error && <div className="bg-red-500 text-white p-2 mb-2">{error}</div>}
-        <h1 className="text-4xl font-bold mb-7">Iniciar sesión</h1>
+        <h1 className="text-4xl font-bold mb-7" style={{ color: 'white' }}>Iniciar sesión</h1>
 
         <label className="text-slate-300">Correo electrónico:</label>
         <input
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -40,7 +40,7 @@ function RegisterPage() {
     <div className="justify-center h-[calc(100vh-4rem)] flex items-center">
       <form onSubmit={handleSubmit} className="bg-neutral-950 px-8 py-10 w-3/12">
         {error && <div className="bg-red-500 text-white p-2 mb-2">{error}</div>}
-        <h1 className="text-4xl font-bold mb-7">Registrarse</h1>
+        <h1 className="text-4xl font-bold mb-7" style={{ color: 'white' }}>Registrarse</h1>
 
         <label className="text-slate-300">Nombre de usuario:</label>
         <input
