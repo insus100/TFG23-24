e10ec3a9157b4c3f868c1caa44dce4a74865d3e2
MigueldeAreba <mdeareba@ucm.es>
2024-02-20 14:42:12 +0100

    modificar perfil completo sin actualizacion en tiempo real

---
+++ src/app/api/auth/emailChange/route.ts
@@ -0,0 +1,52 @@
+import { connectDB } from "@/libs/mongodb";
+import User from "@/models/user";
+import {  NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+
+export async function POST(request: Request){
+
+     const { email } = await request.json();
+  
+    // Validaciones adicionales, si es necesario
+  
+    try {
+        await connectDB();
+  
+      // Obtén el usuario desde la base de datos
+      const session = await getServerSession();
+      const user = await User.findOne({email: session?.user?.email});
+  
+      if (!user) {
+        return NextResponse.json(
+          {
+              message: "Usuario no encontrado"
+          },
+          {
+              status: 404
+          })
+  
+      }
+  
+  
+      // Actualiza la contraseña del usuario en la base de datos
+      user.email = email;
+      await user.save();
+  
+      return NextResponse.json(
+        {
+            message: "Contraseña actualizada con éxito"
+        },
+        {
+            status: 200
+        })
+    } catch (error) {
+      console.error('Error al cambiar la contraseña:', error);
+      return NextResponse.json(
+        {
+            message: "Error interno del servidor"
+        },
+        {
+            status: 500
+        })
+    }
+  };
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import {useSession} from 'next-auth/react'
+import { signOut, useSession} from 'next-auth/react';
 import { useState } from 'react';
 import axios, { AxiosError } from "axios";
 
@@ -15,35 +15,80 @@ function ProfilePage(){
     }
 
     const [newPassword, setNewPassword] = useState('');
+    const [newUsername, setNewUsername] = useState('');
+    const [newEmail, setNewEmail] = useState('');
   
 const handleChangePassword = async () => {
     // Implementa la lógica para cambiar la contraseña utilizando la API de NextAuth
-    const response = await axios.post('/api/auth/password', {
+    const response = await axios.post('/api/auth/passwordChange', {
       password: newPassword,
     });
-  console.log(response);
     if (response.status == 200) {
       console.log('Contraseña actualizada con éxito');
     } else {
       console.error('Error al actualizar la contraseña');
     }
   };
+  
+  const handleChangeUsername = async () => {
+    // Implementa la lógica para cambiar la contraseña utilizando la API de NextAuth
+    const response = await axios.post('/api/auth/usernameChange', {
+      username: newUsername,
+    });
+    if (response.status == 200) {
+      console.log('Nombre de usuario actualizado con éxito');
+    } else {
+      console.error('Error al actualizar el nombre de usuario');
+    }
+  };
+
+  const handleChangeEmail = async () => {
+    // Implementa la lógica para cambiar la contraseña utilizando la API de NextAuth
+    const response = await axios.post('/api/auth/emailChange', {
+      email: newEmail,
+    });
+    if (response.status == 200) {
+      console.log('Email actualizado con éxito');
+    } else {
+      console.error('Error al actualizar el email');
+    }
+  };
 
   return (
     <div>
+      <button className="bg-red-500 text-white px-4 py-2 block" onClick={() => {signOut()}}>Cerrar sesión</button>
+      <button id="profileButton" onClick={() => {irADashboard()}}>Ir al dashboard</button>
       <h1>Perfil de Usuario</h1>
       {session ? (
         <>
           <div>
-            <label>Email:</label>
+            <label>Email Actual:</label>
             <p>{user.email}</p>
           </div>
           <div>
-            <label>Usu:</label>
+            <label>Cambiar Email:  </label>
+            <input
+              type="email"
+              value={newEmail}
+              onChange={(e) => setNewEmail(e.target.value)}
+            />
+            <button onClick={handleChangeEmail}>Guardar Nuevo Email</button>
+          </div>
+          <div>
+            <label>Nombre de Usuario Actual:</label>
             <p>{user.username}</p>
           </div>
           <div>
-            <label>Nueva Contraseña:</label>
+            <label>Cambiar Nombre de Usuario:  </label>
+            <input
+              type="username"
+              value={newUsername}
+              onChange={(e) => setNewUsername(e.target.value)}
+            />
+            <button onClick={handleChangeUsername}>Guardar Nuevo Usuario</button>
+          </div>
+          <div>
+            <label>Cambiar Contraseña:  </label>
             <input
               type="password"
               value={newPassword}
@@ -59,5 +104,8 @@ const handleChangePassword = async () => {
     )
 }
 
+function irADashboard() {
+  window.location.href = '/dashboard'; 
+}
 
 export default ProfilePage
