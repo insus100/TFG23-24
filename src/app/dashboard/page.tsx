1a9ae7864135e968c7a7ec3e93b250e1a62df178
insus100 <inigovelasco@gmail.com>
2024-03-02 21:35:06 +0100

    eliminar eventos..

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,103 +1,108 @@
 "use client"
 import { useEffect, useState } from "react";
-import {signOut, useSession} from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 import { useRouter } from "next/navigation";
 import CalendarTest from "../calendartest/page";
 import { Button, Modal, useDisclosure } from '@nextui-org/react';
 import EventModal from './eventModal'
-import ShowInfoModal  from './showInfoModal'
+import ShowInfoModal from './showInfoModal'
 import axios from 'axios';
 
 
 
-function DashboarPage(){
-    //const _status = await getServerSession(authOptions)
-    const{data: session, status} = useSession()
-    const [events, setEvents] = useState([]);
-    const { isOpen: isEventModalOpen, onOpen: onEventModalOpen, onOpenChange: onEventModalOpenChange } = useDisclosure();
-    const { isOpen: isInfoModalOpen, onOpen: onInfoModalOpen, onOpenChange: onInfoModalOpenChange } = useDisclosure();
-    const router = useRouter();
-    const [selectedEvent, setSelectedEvent] = useState<{ start: Date; end: Date; title: string } | null>(null);
+function DashboarPage() {
+  //const _status = await getServerSession(authOptions)
+  const { data: session, status } = useSession()
+  const [events, setEvents] = useState([]);
+  const { isOpen: isEventModalOpen, onOpen: onEventModalOpen, onOpenChange: onEventModalOpenChange } = useDisclosure();
+  const { isOpen: isInfoModalOpen, onOpen: onInfoModalOpen, onOpenChange: onInfoModalOpenChange } = useDisclosure();
+  const router = useRouter();
+  const [selectedEvent, setSelectedEvent] = useState<any>(null);
 
 
 
 
-    console.log(`session:`, session, `\nstatus:`, status);
-    if(status === 'unauthenticated') {
-        console.log("redirect to register");
-        //return redirect('/login');
-    }
+  console.log(`session:`, session, `\nstatus:`, status);
+  if (status === 'unauthenticated') {
+    console.log("redirect to register");
+    //return redirect('/login');
+  }
 
-    interface EventData {
-        title: string;
-        start: Date; 
-        end: Date;   
+  interface EventData {
+    _id: string;
+    title: string;
+    start: Date;
+    end: Date;
+    creator: any
+  }
+
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get('/api/events/createEvent');
+        console.log("events", response);
+        const fetchedEvents = response.data;
+        const formattedEvents = fetchedEvents.map((event: EventData) => ({
+          _id: event._id,
+          title: event.title,
+          start: new Date(event.start),
+          end: new Date(event.end),
+          creator: event.creator
+        }));
+        setEvents(formattedEvents);
+      } catch (error) {
+        console.error("Error al obtener eventos:", error);
       }
+    })();
+  }, []);
 
-    
-    useEffect(() => {
-        (async () => {
-          try {
-            const response = await axios.get('/api/events/createEvent');
-            console.log("events", response);
-            const fetchedEvents = response.data; 
-            const formattedEvents = fetchedEvents.map((event: EventData) => ({
-              title: event.title,
-              start: new Date(event.start), 
-              end: new Date(event.end),     
-            }));
-            setEvents(formattedEvents);
-          } catch (error) {
-            console.error("Error al obtener eventos:", error);
-          }
-        })();
-      }, []);
-    
-    const irAPerfil = () => {
-        return router.push('/profile')
-    };
+  const irAPerfil = () => {
+    return router.push('/profile')
+  };
 
-    const openEventInfoModal = (event: any) => {
-      setSelectedEvent(event);
-      onInfoModalOpen();
-    }
+  const openEventInfoModal = (event: any) => {
+    setSelectedEvent(event);
+    onInfoModalOpen();
+  }
 
 
 
-    return(
-        <div>DashboardPage
-            <Button color="danger" className="block px-4 py-2" onClick={() => {signOut()}}>
-                Cerrar sesión
-            </Button>
-            <Button color="primary" id="profileButton" onPress={() => {irAPerfil()}}>
-                Ir al Perfil
-            </Button>
-            <Button onPress={onEventModalOpen} className="bg-green-500 text-white px-4 py-2 block mt-4">
-                Agregar Evento al Calendario
-            </Button>
-            <CalendarTest
-              events={events}
-              openEventInfoModal={openEventInfoModal}
-            />
-
-            <Modal 
-                isOpen={isInfoModalOpen} 
-                onOpenChange={onInfoModalOpenChange}
-                placement="top-center"
-            >
-                       {selectedEvent && (
-            <ShowInfoModal selectedEvent={selectedEvent} onClose={() => setSelectedEvent(null)} />
-            )}
-            </Modal>
-            <Modal 
-                isOpen={isEventModalOpen} 
-                onOpenChange={onEventModalOpenChange}
-                placement="top-center"
-            >
-            <EventModal/>
-            </Modal>
-        </div>
-    )
+  return (
+    <div>DashboardPage
+      <Button color="danger" className="block px-4 py-2" onClick={() => { signOut() }}>
+        Cerrar sesión
+      </Button>
+      <Button color="primary" id="profileButton" onPress={() => { irAPerfil() }}>
+        Ir al Perfil
+      </Button>
+      <Button onPress={onEventModalOpen} className="bg-green-500 text-white px-4 py-2 block mt-4">
+        Agregar Evento al Calendario
+      </Button>
+      <div className='justify-center h-[calc(100vh-4rem)] flex items-center'>
+        <CalendarTest
+          events={events}
+          openEventInfoModal={openEventInfoModal}
+        />
+      </div>
+      <Modal
+        isOpen={isInfoModalOpen}
+        onOpenChange={onInfoModalOpenChange}
+        placement="top-center"
+      >
+        {selectedEvent && (
+          <ShowInfoModal selectedEvent={selectedEvent} onClose={() => setSelectedEvent(null)} />
+        )}
+      </Modal>
+      <Modal
+        isOpen={isEventModalOpen}
+        onOpenChange={onEventModalOpenChange}
+        placement="top-center"
+      >
+        <EventModal />
+      </Modal>
+    </div>
+  )
 }
 
 export default DashboarPage
--- src/app/dashboard/showInfoModal.tsx
+++ src/app/dashboard/showInfoModal.tsx
@@ -5,41 +5,52 @@ import axios, { AxiosError } from "axios";
 import { useSession } from 'next-auth/react'
 
 interface showInfoModalProps {
-  selectedEvent: { start: Date; end: Date; title: string } | null;
+  selectedEvent: any;
   onClose: () => void;
 }
 
 export default function ShowInfoModal({ selectedEvent, onClose }: showInfoModalProps) {
   const { data: session, status } = useSession()
   const user = session?.user as any;
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>, closeModal: Function) => {//esto es como hacer function handleSubmit(e: ...) {...}
-    e.preventDefault();
-
+  
+  const eliminarEvento = async (e: any, selEvent: any) => {
+    const res = await axios.post('/api/events/deleteEvent', {
+      _id: selEvent._id
+    });
+    if(res.data.status == 'ok') {
+      //eliminar evento del big calendar y cerrar modal.
+    }
   }
+
   return (
     <>
       {/*<Button onPress={onOpen} color="primary">Open Modal</Button>*/}
       <ModalContent>
         {(onClose) => (
           <>
-            <ModalHeader className="flex flex-col gap-1"  style={{ color: 'white' }}>Información del evento</ModalHeader>
-            <form onSubmit={(e) => handleSubmit(e, onClose)}>
-              <ModalBody style={{ color: 'white' }}>
-            {selectedEvent && (
-              <>
-                <p>Título: {selectedEvent.title}</p>
-                <p>Inicio: {selectedEvent.start.toString()}</p>
-                <p>Fin: {selectedEvent.end.toString()}</p>
-              </>
-            )}
-          </ModalBody>
-          <ModalFooter>
-            <Button color="danger" variant="flat" onPress={onClose}>
-              Cerrar
-            </Button>
-            {/* Puedes agregar más acciones o botones según tus necesidades */}
-          </ModalFooter>
-            </form>
+            <ModalHeader className="flex flex-col gap-1" style={{ color: 'white' }}>Información del evento</ModalHeader>
+
+            <ModalBody style={{ color: 'white' }}>
+              {selectedEvent && (
+                <>
+                  <p>Título: {selectedEvent.title}</p>
+                  <p>Inicio: {selectedEvent.start.toLocaleDateString() + " " + selectedEvent.start.toLocaleTimeString()}</p>
+                  <p>Fin: {selectedEvent.end.toLocaleDateString() + " " + selectedEvent.end.toLocaleTimeString()}</p>
+                  <p>Creador: {selectedEvent.creator.username}</p>
+                </>
+              )}
+            </ModalBody>
+            <ModalFooter>
+              {selectedEvent && user && selectedEvent.creator._id === user._id &&
+                (<>
+                  <Button color="danger" variant="flat" onPress={(e) => eliminarEvento(e, selectedEvent)}>
+                    Eliminar
+                  </Button>
+                </>)
+              }
+
+              {/* Puedes agregar más acciones o botones según tus necesidades */}
+            </ModalFooter>
           </>
         )}
       </ModalContent>
